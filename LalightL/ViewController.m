a5963bacb0ee2361a485442e88d8d02e16d619ea
manguoge <312825660@qq.com>
2017-03-21 22:02:39 +0800

    Initial Commit

---
+++ LalightL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LalightL
+//
+//  Created by comfouriertech on 17/3/21.
+//  Copyright © 2017年 ronghua_li. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
